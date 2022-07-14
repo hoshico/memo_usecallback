c1889a5efd44123ae90935b97e11c9b5b32766ca
hoshico <melancholinista.yasu@gmail.com>
2022-07-15 08:03:45 +0900

    add status

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,22 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
-  return <div className={styles.container}>
-    
-  </div>;
+export default function Home<NextPage> () {
+  const [ status, setStatus ] = useState<number>(0);
+  const incrementHandler = () => {
+    setStatus((pre) => pre + 1);
+  };
+  return (
+    <div className={styles.container}>
+      <p>カウント: {status}</p>
+      <button onClick={incrementHandler}>+</button>
+    </div>)
 };
 
-export default Home;
+
+
+
+
+
+
